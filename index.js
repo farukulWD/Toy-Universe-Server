bb9c032214efafd06e05c75e838cca7b9b0117b8
Farukul Islam <farukgb1999@gmail.com>
2023-05-17 23:35:25 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+const app = express()
+
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.listen(port,()=>{
+    console.log(`The Toy universe server is running ${port}`);
+})
+
