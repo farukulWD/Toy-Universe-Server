b04102a76e6a63f75ad7326bd09e895f07d9386c
Farukul Islam <farukgb1999@gmail.com>
2023-05-19 14:50:42 +0600

    write get method for silngle toy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri =`mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.0w2mrif.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.0w2mrif.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,26 +28,43 @@ async function run() {
 
     const toyCollection = client.db("toyDB").collection("toyos");
 
-    app.post("/addtoy",async(req,res)=>{
-        const body =req.body;
-        const result = await toyCollection.insertOne(body)
+    app.post("/addtoy", async (req, res) => {
+      const body = req.body;
+      console.log(body);
+      const result = await toyCollection.insertOne(body);
+      res.send(result);
+    });
+
+    app.get("/alltoy", async (req, res) => {
+      const result = await toyCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    app.get("/getJobByCategory/:category", async (req, res) => {
+      const category = req.params.category;
+      if (category === "all") {
+        const result = await toyCollection.find({}).toArray();
+        return res.send(result);
+      } else {
+        const result = await toyCollection
+          .find({ sub_category: category })
+          .toArray();
+       return res.send(result);
+      }
+    });
+
+
+    app.get("/myCar/:email",async(req,res)=>{
+        const result = await toyCollection.find({seller_email: req.params.email}).toArray();
         res.send(result)
     })
 
-
-    app.get("/alltoy",async(req,res)=>{
-        const result = await toyCollection.find({}).toArray()
+    app.get("/singleToy/:id",async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await toyCollection.findOne(query)
         res.send(result)
     })
-
-
-    app.get("/getJobByCategory/:category",async(req,res)=>{
-       const result = await toyCollection.find({sub_category: req.params.category}).toArray();
-       res.send(result)
-    })
-
-
-
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -60,10 +77,9 @@ async function run() {
   }
 }
 run().catch(console.dir);
-app.get("/",(req,res)=>{
-    res.send("Toy universe running")
-    
-})
+app.get("/", (req, res) => {
+  res.send("Toy universe running");
+});
 
 app.listen(port, () => {
   console.log(`The Toy universe server is running ${port}`);
