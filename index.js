2e29b087c2c8123908e66aa47c5a04e9e09516a9
Farukul Islam <farukgb1999@gmail.com>
2023-05-19 11:24:30 +0600

    environment varriable setup

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config()
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,26 +10,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-import { MongoClient } from "mongodb";
+const uri =`mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.0w2mrif.mongodb.net/?retryWrites=true&w=majority`;
 
-// Replace the uri string with your MongoDB deployment's connection string.
-const uri = "<connection string uri>";
-
-const client = new MongoClient(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
   try {
-    const database = client.db("insertDB");
-    const haiku = database.collection("haiku");
-    // create a document to insert
-    const doc = {
-      title: "Record of a Shriveled Datum",
-      content: "No bytes, no problem. Just insert a document, in MongoDB",
-    };
-    const result = await haiku.insertOne(doc);
-
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
+    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
