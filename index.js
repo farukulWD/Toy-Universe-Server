fc09411964128031ec4a254ae8f143694c6f736e
Farukul Islam <farukgb1999@gmail.com>
2023-05-19 11:12:04 +0600

    Mongodb setup

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const port = process.env.PORT || 5000;
-const app = express()
-
-
+const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
+import { MongoClient } from "mongodb";
 
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri = "<connection string uri>";
 
-app.listen(port,()=>{
-    console.log(`The Toy universe server is running ${port}`);
-})
+const client = new MongoClient(uri);
 
+async function run() {
+  try {
+    const database = client.db("insertDB");
+    const haiku = database.collection("haiku");
+    // create a document to insert
+    const doc = {
+      title: "Record of a Shriveled Datum",
+      content: "No bytes, no problem. Just insert a document, in MongoDB",
+    };
+    const result = await haiku.insertOne(doc);
+
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`The Toy universe server is running ${port}`);
+});
